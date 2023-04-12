72b3019de135316c2f49a86ff749b54d6895658b
olumideogundele1 <ogundeleolumuyiwaolumide@gmail.com>
2023-04-12 23:52:19 +0100

    open light box container grom gallery image component

--- src/components/gallery/LightBox.js
+++ src/components/gallery/LightBox.js
@@ -1,5 +1,10 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useContext } from "react";
+import GalleryContext from "../../context/GalleryContext";
 const LightBox = () => {
+  const {
+    galleryData: { currentLightBox },
+  } = useContext(GalleryContext);
   return (
     <div className="gallery__lightbox">
       <h4>Nigeria</h4>
